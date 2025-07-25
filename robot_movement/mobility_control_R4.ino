6253f33a017fc7027b0cd6a44b1fe7926c9e7083
Sonia Liu <sonia.liuys@gmail.com>
2025-07-25 22:55:29 +0800

    update R4 with joystick control

--- robot_movement/mobility_control_R4.ino
+++ robot_movement/mobility_control_R4.ino
@@ -1,31 +1,149 @@
 #include "MatrixMiniR4.h"
 
+void setup()
+{
+  MiniR4.begin();
+  MiniR4.PWR.setBattCell(2);
+  Serial.begin(9600);
+  MiniR4.PS2.read_gamepad(false, 0);
+}
 
+void loop()
+{
+  if(MiniR4.PS2.Button(PSB_PAD_UP))
+  {
+    MiniR4.M1.setPower(200);
+    MiniR4.M2.setPower(200);
+    MiniR4.M3.setPower(200);
+    MiniR4.M4.setPower(200);
+    delay(1000);
+    MiniR4.M1.setPower(0);
+    MiniR4.M2.setPower(0);
+    MiniR4.M3.setPower(0);
+    MiniR4.M4.setPower(0);
+  }
+  if(MiniR4.PS2.Button(PSB_PAD_DOWN))
+  {
+    MiniR4.M1.setPower((-200));
+    MiniR4.M2.setPower((-200));
+    MiniR4.M3.setPower((-200));
+    MiniR4.M4.setPower((-200));
+    delay(1000);
+    MiniR4.M1.setPower(0);
+    MiniR4.M2.setPower(0);
+    MiniR4.M3.setPower(0);
+    MiniR4.M4.setPower(0);
+  }
+  if(MiniR4.PS2.Button(PSB_PAD_RIGHT))
+  {
+    MiniR4.M1.setPower(200);
+    MiniR4.M2.setPower((-200));
+    MiniR4.M3.setPower(200);
+    MiniR4.M4.setPower((-200));
+    delay(1000);
+    MiniR4.M1.setPower(0);
+    MiniR4.M2.setPower(0);
+    MiniR4.M3.setPower(0);
+    MiniR4.M4.setPower(0);
+  }
+  if(MiniR4.PS2.Button(PSB_PAD_LEFT))
+  {
+    MiniR4.M1.setPower((-200));
+    MiniR4.M2.setPower(200);
+    MiniR4.M3.setPower((-200));
+    MiniR4.M4.setPower(200);
+    delay(1000);
+    MiniR4.M1.setPower(0);
+    MiniR4.M2.setPower(0);
+    MiniR4.M3.setPower(0);
+    MiniR4.M4.setPower(0);
+  }
+
+}
+// 下一個
+#include "MatrixMiniR4.h"
 
 void setup()
 {
   MiniR4.begin();
   MiniR4.PWR.setBattCell(2);
   Serial.begin(9600);
-  MiniR4.I2C1.MXLaser.begin();
+  MiniR4.PS2.read_gamepad(false, 0);
   MiniR4.M1.setReverse(false);
   MiniR4.M2.setReverse(true);
-  delay(100);
+  MiniR4.M3.setReverse(false);
+  MiniR4.M4.setReverse(true);
 }
 
 void loop()
 {
-  MiniR4.M1.setPower(60);
-  MiniR4.M2.setPower(60);
-  delay(2000);
-  MiniR4.M1.setPower(0);
-  MiniR4.M2.setPower(0);
-  delay(200);
-  MiniR4.M1.setPower(60);
-  MiniR4.M2.setPower((-60));
-  delay(500);
-  MiniR4.M1.setPower(0);
-  MiniR4.M2.setPower(0);
-  delay(200);
+  if(MiniR4.PS2.Button(PSB_L1))
+  {
+    for(int i_0 = 0; i_0 < 4; i_0++)
+    {
+      MiniR4.M1.setPower(200);
+      MiniR4.M2.setPower(200);
+      MiniR4.M3.setPower(200);
+      MiniR4.M4.setPower(200);
+      delay(2000);
+      MiniR4.M1.setPower(0);
+      MiniR4.M2.setPower(0);
+      MiniR4.M3.setPower(0);
+      MiniR4.M4.setPower(0);
+      delay(200);
+      MiniR4.M1.setPower(200);
+      MiniR4.M2.setPower((-200));
+      MiniR4.M3.setPower(200);
+      MiniR4.M4.setPower((-200));
+      delay(500);
+      MiniR4.M1.setPower(0);
+      MiniR4.M2.setPower(0);
+      MiniR4.M3.setPower(0);
+      MiniR4.M4.setPower(0);
+      delay(200);
+    }
+  }
+
+}
+// 下一個
+#include "MatrixMiniR4.h"
+
+void setup()
+{
+  MiniR4.begin();
+  MiniR4.PWR.setBattCell(2);
+  Serial.begin(9600);
+  MiniR4.I2C1.MXLaserV2.begin();
+}
+
+void loop()
+{
+  if(MiniR4.I2C1.MXLaserV2.getDistance() < 10)
+  {
+    MiniR4.M1.setPower(0);
+    MiniR4.M2.setPower(0);
+  }
+
+}
+// 下一個oo
+#include "MatrixMiniR4.h"
+
+void setup()
+{
+  MiniR4.begin();
+  MiniR4.PWR.setBattCell(2);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(MiniR4.A1.getAIL() < 500)
+  {
+    Serial.print("Dark");
+  }
+  else
+  {
+    Serial.print("Bright");
+  }
 
 }
